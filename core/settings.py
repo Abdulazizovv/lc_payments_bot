92ae7cbe28a70ac51dbeb09999314861fb51d531
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2025-09-16 16:42:37 +0500

    changed port

--- core/settings.py
+++ core/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:8080",
-    "http://localhost:8080",
+    "http://127.0.0.1:8088",
+    "http://localhost:8088",
 ]
 
 
